ac6f0b393c0aaeab2518183285a93ada03a73bcd
Imad Baber <imadbaber@yahoo.com>
2017-10-02 23:44:03 -0400

    first update

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,11 @@
-$("#start").on('click',function(){
+$('#start').on('click',function(){
 	$('#start').remove();
 	for(var i = 0; i<questions.length;i++){
-		$('#subwarpper').append('<h2'+questions[i].question+'</h2');
-		// for (var j=0; j<questions[i].length;j++)
-		// $("#subwarpper").append("<input type='radio' ")
+		$('#subwrapper').append('<h2>'+questions[i].question+'</h2>');
+		 for (var j=0; j<questions[i].answers.length;j++){
+			 $("#subwrapper").append("<input type='radio' name= 'question-'"+i+"' value = ' "+questions[i].answers[j]+"'>"+questions[i].answers[j])
+		}
 	}
-
 })
 
 var questions = [{
@@ -13,14 +13,12 @@ var questions = [{
 	answers: ["Justin Timberlake", "Elvis Presley", "Jay-Z", "Michael Jackson", "Usher"],
 	correctAnswer: "Michael Jackson"
 }, {
-	question: "Which team drafted Kobe Bryant?"
+	question: "Which team drafted Kobe Bryant?",
 	answers: ["Miami Heat", "LA Lakers", "Charlotte Hornets", "LA Clippers", "Charlotte Bobcats"],
 	correctAnswer: "Charlotte Hornets"
 }, {
-	question: "Which team won the NBA finals in 2011?"
+	question: "Which team won the NBA finals in 2011?",
 	answers: ["LA Lakers", "Miami Hornets", "Dallas Mavericks", "Boston Celtics", "trick question"],
 	correctAnswer:"Dallas Mavericks"
-}
-
 }];
 
